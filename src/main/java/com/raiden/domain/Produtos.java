43836e99865984de65f023daf9b574657c45f90f
Pablo <58537127+PabloaRuiz@users.noreply.github.com>
2021-09-20 22:52:04 -0300

    Endpoint /pedido/{id} disponibilizado #11

--- src/main/java/com/raiden/domain/Produtos.java
+++ src/main/java/com/raiden/domain/Produtos.java
@@ -16,6 +16,7 @@ import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Produtos implements Serializable {
@@ -49,6 +50,7 @@ public class Produtos implements Serializable {
 		this.preco = preco;
 	}
 	
+	@JsonIgnore
 	public List<Pedidos> getPedidos() {
 		List<Pedidos> lista = new ArrayList<>();
 		for(ItemPedido x : itens) {
